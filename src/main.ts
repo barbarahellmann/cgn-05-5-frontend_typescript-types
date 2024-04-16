b741bea31e76110c2aee1d7bebf2607ecd2f5d68
barbarahellmann <babshellmann@web.de>
2024-04-16 10:27:30 +0200

    Done

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,45 @@ const person: Person2 = {
     isAdmin:true
 }
 
+// Literals = übergeben mehrere Werte
+
+type TrafficLightLights = "red" | "yellow" | "green"
+
+    let currentTrafficLights: TrafficLightLights = "green"   // Literals verwenden
+
+
+// Calculator example
+
+type Operator = "add" | "subtract" | "multiply" | "divide"  // Types werden immer groß geschrieben
+
+
+type Input = {  // Deklarierung der Zahl
+    operator:Operator;
+    operands: [number, number]  // number kann auch Komma-Zahlen enthalten
+}
+
+const input: Input =  {  // Übergabe der Werte
+    operator: "add",
+    operands: [5,3]
+}
+
+function calculate(input:Input): number { //Funktion
+    switch (input.operator) {
+        case "add":
+            return input.operands[0] + input.operands[1];
+        case "subtract":
+            return input.operands[0] + input.operands[1];
+        case "multiply":
+            return input.operands[0] + input.operands[1];
+        case "divide":
+            return input.operands[0] + input.operands[1];
+        default:
+            throw new Error("Invalid operator")
+    }
+}
+
+const result: number = calculate(input); // Ausgabe der Ergebnisses
+console.log("Result:", result);
+
+
+
